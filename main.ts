a866a9eb15c98f496c87607293777a307b641689
Filipe Kiss <hello@filipekiss.com.br>
2021-10-19 23:37:00 +0200

    feat: Add quote image generation

--- main.ts
+++ main.ts
@@ -1,23 +1,39 @@
-import { Bot } from "grammy";
-import { ARCHIVE_CHANNEL_ID, BOT_TOKEN, BOT_USERNAME } from "./environment"
-
+import { Bot, InputFile } from "grammy";
+import { ARCHIVE_CHANNEL_ID, BOT_TOKEN, BOT_USERNAME } from "./environment";
+import { generateQuoteImage } from "./helpers/generate-quote-image";
 
 const bot = new Bot(BOT_TOKEN);
+const botUsername = BOT_USERNAME.toLowerCase();
 
-bot.on("::mention", ctx => {
+bot.on("::mention", async (ctx) => {
   console.log("Bot was mentioned");
   const receivedMessage = ctx.update.message;
-  const isPureMention = ctx.update.message?.text === BOT_USERNAME;
+  const isPureMention = ctx.update.message?.text?.toLowerCase() === botUsername;
 
   if (isPureMention && receivedMessage && receivedMessage.reply_to_message) {
-    console.log("The message was a reply, forwarding to archive channel")
+    console.log("The message was a reply, forwarding to archive channel");
     const messageToQuote = receivedMessage.reply_to_message;
     console.log(messageToQuote);
-    ctx.api.forwardMessage(ARCHIVE_CHANNEL_ID, receivedMessage.chat.id, messageToQuote.message_id);
+    ctx.api.forwardMessage(
+      ARCHIVE_CHANNEL_ID,
+      receivedMessage.chat.id,
+      messageToQuote.message_id
+    );
+    if (messageToQuote.text != undefined) {
+      ctx.api.sendPhoto(
+        ARCHIVE_CHANNEL_ID,
+        new InputFile(
+          await generateQuoteImage({
+            text: messageToQuote.text,
+            author:
+              messageToQuote.from!.username ?? messageToQuote.from!.first_name,
+          })
+        )
+      );
+    }
   }
-
-})
+});
 
 bot.start();
 
-console.log("Starting")
+console.log("Starting");
