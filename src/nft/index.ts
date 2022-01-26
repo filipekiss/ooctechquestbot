8df9270cbbd809e1f3a09557038fa43707790abf
Filipe Kiss <hello@filipekiss.com.br>
2022-01-26 12:03:10 +0100

    feat(nft): add new meanings

--- src/nft/index.ts
+++ src/nft/index.ts
@@ -8,10 +8,15 @@ const  NFT_Meanings = [
   "Nem Fudendo, Tapado",
   "Nove Filas de Tatu",
   "Novos Filhos de Thanos",
+  "Não Funciona o Testículo",
+  "Necessidade de Fazer Terapia",
+  "Nunca Fiz Tatuagem",
+  "Never Felt Titties",
+  "Need Female Touch"
 ];
 
 export const nft = new Composer<OocContext>();
-nft.hears(/nft/, async (ctx) => {
+nft.hears(/nft/i, async (ctx) => {
   const receivedMessage = ctx.message!;
   const nftMeandng = NFT_Meanings[Math.floor(Math.random()*NFT_Meanings.length)];
   const botReply = await ctx.reply(`NFT significa "${nftMeandng}"`,
