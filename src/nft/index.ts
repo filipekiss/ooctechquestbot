6a0e8862d35bd615016ba8619a2164bb711043ee
Filipe Kiss <hello@filipekiss.com.br>
2022-02-11 22:41:58 +0100

    feat(nft): Add /mint command
    Now the users can add new NFT meanings. All it's needed is to send a
    message in the format `/mint <meaning>`, where <meaning> is a string
    that satisfies having at least three words and starting with N,F,T,
    respectively

--- src/nft/index.ts
+++ src/nft/index.ts
@@ -1,5 +1,8 @@
 import { Composer } from "grammy";
 import { OocContext } from "../config";
+import { DB_FOLDER } from "../config/environment";
+import Keyv from "keyv";
+import { ReplyMessage } from "@grammyjs/types";
 
 const NFT_Meanings = [
   "Não Faço Trabalho",
@@ -20,9 +23,118 @@ const NFT_Meanings = [
 export const nft = new Composer<OocContext>();
 nft.hears(/nft/i, async (ctx) => {
   const receivedMessage = ctx.message!;
+  const definitionListStr = (await nftDB.get(
+    NFT_SCHEMA.NFT_MESSAGES
+  )) as string;
+  const definitionList = definitionListStr ? definitionListStr.split("\n") : [];
+  const allMeanings = [...NFT_Meanings, ...definitionList];
+  console.log(allMeanings)
   const nftMeaning =
-    NFT_Meanings[Math.floor(Math.random() * NFT_Meanings.length)];
+    allMeanings[Math.floor(Math.random() * allMeanings.length)];
   return await ctx.reply(`NFT significa "${nftMeaning}"`, {
     reply_to_message_id: receivedMessage.message_id,
   });
+});
+
+type NftDefinition = {
+  n: string;
+  f: string;
+  t: string;
+  message: ReplyMessage;
+};
+
+const NFT_SCHEMA = {
+  NFT_MESSAGES: "nft_messages",
+};
+
+const nftDB = new Keyv(`sqlite://${DB_FOLDER}/nft.sqlite`);
+
+const isValidMeaning = (x: string, startWith: string) =>
+  x.toLowerCase().startsWith(startWith);
+const titleCase = (word: string) => `${word[0].toUpperCase()}${word.slice(1)}`;
+
+const validateNftMeaning = (n: string, f: string, t: string) => {
+  const isNValid = isValidMeaning(n, "n");
+  const isFValid = isValidMeaning(f, "f");
+  const isTValid = isValidMeaning(t, "t");
+  return isNValid && isFValid && isTValid;
+};
+
+const joinNft = ({ n, f, t }: NftDefinition) => `${n} ${f} ${t}`;
+
+const addDefinition = async (definition: NftDefinition) => {
+  const definitionListStr = (await nftDB.get(
+    NFT_SCHEMA.NFT_MESSAGES
+  )) as string;
+  const definitionList = definitionListStr ? definitionListStr.split("\n") : [];
+  await nftDB.set(joinNft(definition), definition);
+  definitionList.push(joinNft(definition));
+  await nftDB.set(NFT_SCHEMA.NFT_MESSAGES, definitionList.join("\n"));
+};
+
+async function replyAlreadyAdded(ctx: OocContext) {
+  const receivedMessage = ctx.message!;
+  const botReply = await ctx.reply("Essa definição já existe.", {
+    reply_to_message_id: receivedMessage.message_id,
+  });
+  setTimeout(async () => {
+    try {
+      await receivedMessage.delete();
+      await botReply.delete();
+    } catch {
+      console.warn("Unable to delete message. Skipping…");
+    }
+  }, 15000);
+  return;
+}
+
+async function replyInvalidMeaning(ctx: OocContext) {
+  const receivedMessage = ctx.message!;
+  const botReply = await ctx.reply(
+    `${ctx.match} não é um significado válido para NFT`,
+    {
+      reply_to_message_id: receivedMessage.message_id,
+    }
+  );
+  setTimeout(async () => {
+    try {
+      await receivedMessage.delete();
+      await botReply.delete();
+    } catch {
+      console.warn("Unable to delete message. Skipping…");
+    }
+  }, 15000);
+  return;
+}
+
+nft.command("mint", async (ctx) => {
+  const [n, f, t] = ctx.match.split(" ");
+  const isValidNFT = validateNftMeaning(n, f, t);
+  if (!isValidNFT) {
+    return replyInvalidMeaning(ctx);
+  }
+  const definition = {
+    n: titleCase(n),
+    f: titleCase(f),
+    t: titleCase(t),
+    message: ctx.message as ReplyMessage,
+  };
+  const existingDefinition = await nftDB.get(joinNft(definition));
+  if (existingDefinition || NFT_Meanings.includes(joinNft(definition))) {
+    return replyAlreadyAdded(ctx);
+  }
+  await addDefinition(definition);
+  const receivedMessage = ctx.message!;
+  const botReply = await ctx.reply(`Definição adicionada`, {
+    reply_to_message_id: receivedMessage.message_id,
+  });
+  setTimeout(async () => {
+    try {
+      await receivedMessage.delete();
+      await botReply.delete();
+    } catch {
+      console.warn("Unable to delete message. Skipping…");
+    }
+  }, 15000);
+  return;
 });
