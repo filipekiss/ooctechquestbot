704de61678cbb4f9196c56808fa691b65a70832b
Filipe Kiss <hello@filipekiss.com.br>
2022-05-02 12:11:27 +0200

    fix(quote): Change how acende and the quote command works

--- src/acende/index.ts
+++ src/acende/index.ts
@@ -1,14 +1,10 @@
-import {Composer} from 'grammy'
-import {Animation, createAnimatedMessage} from '../animations/helpers';
-import {OocContext} from '../config';
+import { Composer } from "grammy";
+import { Animation, createAnimatedMessage } from "../animations/helpers";
+import { OocContext } from "../config";
 
 const fireworksAnimation: Animation = [[["🧨"]], [["🎆"]]];
 
 export const acende = new Composer<OocContext>();
 acende.command("acende", createAnimatedMessage(fireworksAnimation));
-acende.on("message:text", (ctx) => {
-  if (ctx.message.text.toLowerCase() === "/acende@papocobot") {
-    createAnimatedMessage(fireworksAnimation)(ctx);
-  }
-});
-
+const pattern = /\/acende\@papocobot/gim;
+acende.hears(pattern, createAnimatedMessage(fireworksAnimation));
--- src/ooc/index.ts
+++ src/ooc/index.ts
@@ -7,38 +7,44 @@ import { removeSurroundingQuotes } from "./remove-surrounding-quotes";
 export const ooc = new Composer();
 
 const botUsername = BOT_USERNAME.toLowerCase();
-ooc.on("message:text").on("::mention", async (ctx) => {
+ooc.on("message:text", async (ctx) => {
   console.log("Bot was mentioned");
   const receivedMessage = ctx.update.message;
   const isPureMention = receivedMessage.text
     .toLowerCase()
-    .startsWith(botUsername);
+    .startsWith(`@${botUsername}`);
 
-  if (isPureMention && receivedMessage && receivedMessage.reply_to_message) {
+  const messageToQuote = receivedMessage.reply_to_message;
+  if (isPureMention && receivedMessage && messageToQuote) {
     console.log("The message was a reply, forwarding to archive channel");
     const [action] = parseArguments(receivedMessage.text!);
-    const messageToQuote = receivedMessage.reply_to_message;
     console.log(messageToQuote);
-    ctx.api.forwardMessage(
-      ARCHIVE_CHANNEL_ID,
-      receivedMessage.chat.id,
-      messageToQuote.message_id
-    );
-    if (messageToQuote.text !== undefined) {
-      const quoteText = removeSurroundingQuotes(messageToQuote.text);
-      const generatedImage = await generateQuoteImage({
-        text: quoteText,
-        author:
-          messageToQuote.from!.username ?? messageToQuote.from!.first_name,
-        query: action,
-      });
-      if (generatedImage) {
-        return ctx.api.sendPhoto(
-          ARCHIVE_CHANNEL_ID,
-          new InputFile(generatedImage)
-        );
+    try {
+      await ctx.api.forwardMessage(
+        ARCHIVE_CHANNEL_ID,
+        receivedMessage.chat.id,
+        messageToQuote.message_id
+      );
+    } catch (e) {
+      console.log("Message not found, not forwarded");
+    } finally {
+      if (messageToQuote.text !== undefined) {
+        console.log("Message has text, generating quote");
+        const quoteText = removeSurroundingQuotes(messageToQuote.text);
+        const generatedImage = await generateQuoteImage({
+          text: quoteText,
+          author:
+            messageToQuote.from!.username ?? messageToQuote.from!.first_name,
+          query: action,
+        });
+        if (generatedImage) {
+          return ctx.api.sendPhoto(
+            ARCHIVE_CHANNEL_ID,
+            new InputFile(generatedImage)
+          );
+        }
+        return true;
       }
-      return true;
     }
   }
 });
